5c186bad92fc984f438fe3b8c9b335cc17849a9a
Marina Sanchez <masaro1411@gmail.com>
2020-11-02 11:35:19 -0600

    se creo el login y registro de firebse

---
+++ src/app/pages/registro/registro.component.ts
@@ -0,0 +1,54 @@
+import { Component, OnInit } from '@angular/core';
+import { UsuarioModel } from '../../models/usuario.model';
+import { NgForm } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
+import Swal from 'sweetalert2';
+import { Router } from '@angular/router';
+import 'sweetalert2/src/sweetalert2.scss';
+
+@Component({
+  selector: 'app-registro',
+  templateUrl: './registro.component.html',
+  styleUrls: ['./registro.component.css']
+})
+export class RegistroComponent implements OnInit {
+
+  usuario: UsuarioModel;
+  recordarme = false;
+
+  constructor(private auth: AuthService,
+    private router: Router) { }
+
+  ngOnInit() { 
+    this.usuario = new UsuarioModel();
+  }
+
+  onSubmit(form: NgForm ) {
+    if (form.invalid) { return; }
+
+    Swal({
+      buttons:{value: false},
+      icon: 'info',
+      text: 'Espere por favor...',
+      closeOnClickOutside: false,
+    });
+
+    this.auth.nuevousuario(this.usuario)
+    .subscribe(resp => {
+      console.log(resp);
+      Swal.close();
+      if (this.recordarme) {
+        localStorage.setItem('email', this.usuario.email);
+      }
+      this.router.navigateByUrl('/home');
+    }, (err) => {
+      console.log(err.error.error.message);
+      Swal.fire({
+        icon: 'error',
+        title: 'Error al registrar',
+        text: err.error.error.message
+      });
+    });
+
+  }
+}
